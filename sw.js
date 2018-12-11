6a03a1b901fe94bb6659440d3e72d2f22ac31b1b
Moisés <moises.cid@ideit.es>
2018-12-11 13:28:12 +0100

    Primer commit

---
+++ sw.js
@@ -0,0 +1,21 @@
+const STATIC_CACHE = 'static-v1';
+const DYNAMIC_CACHE = 'dinamic-v1';
+const INMUTABLE_CACHE = 'inmutable-v1';
+
+
+const APP_SHELL = [
+    '/',
+    'index.html',
+    '/img/avatars/hulk.jpg',
+    '/img/avatars/ironman.jpg',
+    '/img/avatars/spiderman.jpg',
+    '/img/avatars/thor.jpg',
+    '/img/avatars/wolverine.jpg',
+    'css/style.css',
+    'js/app.js'
+];
+
+
+const APP_SHELL_INMUTABLE = [
+
+]
